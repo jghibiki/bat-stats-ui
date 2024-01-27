5491500d9d61d652127f99bb0bcf0ee938cf6ce0
jghibiki <jghibiki.games@gmail.com>
2024-01-27 14:13:22 -0600

    Add pagination

--- src/routes/card_viewer_test.tsx
+++ src/routes/card_viewer_test.tsx
@@ -1,6 +1,6 @@
-import { Container } from "@suid/material"
+import { Box, CircularProgress, Container, Typography } from "@suid/material"
 import CharacterCard from "../components/character_card"
-import { onMount, createSignal } from "solid-js";
+import { Show, createEffect, createSignal } from "solid-js";
 import { For } from "solid-js";
 import { Grid, Button } from "@suid/material"
 import { GameDataService } from "../service/game_data_service";
@@ -10,30 +10,97 @@ export default function CardViewerTest() {
 
     const [characters, setCharacters] = createSignal<Array<CharacterModel>>([])
     const [compactMode, setCompactMode] = createSignal<Boolean>(true)
+    const [page, setPage] = createSignal(1)
+    const [maxPage, setMaxPage] = createSignal<null | number>(null)
+    const [totalResults, setTotalResults] = createSignal<null | number>(null)
+    const [loading, setLoading] = createSignal(true)
 
-    onMount(async () => {
+    createEffect(async () => {
         const game_data_service = GameDataService.getInstance()
-        let result = await game_data_service.getCharactersByPage()
+        setLoading(true)
+        let result = await game_data_service.getCharactersByPage(null, page())
         let nonEternalCharacters = result.data.filter(x => !x.eternal)
         setCharacters(
             nonEternalCharacters
         )
+        setMaxPage(
+            result.total_pages
+        )
+        setTotalResults(
+            result.total_pages
+        )
+        setLoading(false)
     })
+
+    const hasPreviousPage = () => {
+        return page() > 1
+    }
+
+    const hasNextPage = () => {
+        return page() < maxPage()
+    }
+
+    const nextPage = () => {
+        setPage(x => x + 1)
+    }
+
+    const previousPage = () => {
+        setPage(x => x - 1)
+    }
 
     const toggleCompactMode = () => {
         setCompactMode(!compactMode())
     }
 
-    return <div>
-        <Button variant="contained" onClick={toggleCompactMode} sx={{ marginBottom: 1 }}>Toggle Compact</Button>
-
-        <Grid container spacing={2} justifyContent="center">
-            <For each={characters().splice(0, 20)}>{(character, i) =>
+    return <Box>
+        <Grid container sx={{ marginBottom: 1 }}>
+            <Grid item xs>
+                <Button
+                    variant="contained"
+                    onClick={toggleCompactMode}
+                >
+                    Toggle Compact
+                </Button>
+            </Grid>
+            <Grid item xs />
+            <Grid item xs={3} container spacing={1} justifyContent="center">
                 <Grid item>
-                    <CharacterCard character={character} compactMode={compactMode()} />
+                    <Button
+                        variant="contained"
+                        onClick={previousPage}
+                        disabled={!hasPreviousPage()}
+                    >
+                        Previous Page
+                    </Button>
                 </Grid>
-            }</For>
+                <Grid item>
+                    <Typography variant={"body1"} color="white">
+                        {page()} / {maxPage()}
+                    </Typography>
+                </Grid>
+                <Grid item>
+                    <Button
+                        variant="contained"
+                        onClick={nextPage}
+                        disabled={!hasNextPage()}
+                    >
+                        Next Page
+                    </Button>
+                </Grid>
+            </Grid>
         </Grid>
-    </div>
+
+        <Show when={!loading()}
+            fallback={<CircularProgress />}
+        >
+            <Grid container spacing={1} justifyContent="center">
+                <For each={characters().splice(0, 20)}>{(character, i) =>
+                    <Grid item>
+                        <CharacterCard character={character} compactMode={compactMode()} />
+                    </Grid>
+                }</For>
+            </Grid>
+        </Show>
+    </Box>
 
 }
